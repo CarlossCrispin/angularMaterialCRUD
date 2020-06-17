a5fcb9aa22e58e5597ad7a8fc798278e4a8fa163
CarlossCrispin <carlos.crispin.cc@gmail.com>
2020-06-17 15:32:03 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyAfl_ZkBmZdCNs3khUYbLq9avywnnugCPk',
-    authDomain: 'customers-2020.firebaseapp.com',
-    databaseURL: 'https://customers-2020.firebaseio.com',
-    projectId: 'customers-2020',
-    storageBucket: 'customers-2020.appspot.com',
-    messagingSenderId: '332994246324',
-    appId: '1:332994246324:web:59b805b5054977335cc9e1',
-    measurementId: 'G-ZFXCNE0CJ4'
+    
   }
 };
